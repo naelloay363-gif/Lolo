59357f137bcfff1bc8cddc8d6cc775482ad30feb
naelloay363-gif <naelloay363@gmail.com>
2025-10-27 23:23:03 +0200

    Change service worker version to v3
    Updated service worker version from v8 to v3.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js — قوي ومرن لمجلدات GitHub Pages الفرعية
-const VERSION = "v8"; // زِد الرقم كل ما تحدث ملفاتك
+const VERSION = "v3"; // زِد الرقم كل ما تحدث ملفاتك
 const BASE = new URL(self.registration.scope).pathname; // مثال: "/Lolo/"
 const CACHE_NAME = `lolo-cache-${VERSION}`;
 
